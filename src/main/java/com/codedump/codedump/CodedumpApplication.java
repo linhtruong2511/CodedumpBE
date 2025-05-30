353821d8ef8f4f5a792cf3202ec72b128867dc68
linhtruong <linhtk2511044@gmail.com>
2025-05-30 08:12:16 +0700

    init project

---
+++ src/main/java/com/codedump/codedump/CodedumpApplication.java
@@ -0,0 +1,13 @@
+package com.codedump.codedump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodedumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodedumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codedump/codedump/CodedumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedump.codedump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
